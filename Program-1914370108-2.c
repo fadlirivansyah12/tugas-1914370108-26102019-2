06ed9b78f9716e76d6160ca9098bd32a166761f7
fadlirivansyah12 <55773490+fadlirivansyah12@users.noreply.github.com>
2019-10-31 22:32:48 +0700

    Add files via upload

---
+++ Program-1914370108-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    if (a > 0){
+        printf ("D= b*2 - (4*a*c)")
+    } else {
+    if ("D>=0"){
+        printf ("x1=(-b+√D)/2*a");
+        scanf ("x2=(-b+√D)/2*a");
+        printf ("print x1,x2");
+     printf("print akar imajiner");
+    }
+    return 0;
+  }}
